2aa0d1c367cecaa099c5b9285308e09cc21b86dd
Mehrdad Kazemi <Mehrdad.Kazemi@ac-e.be>
2020-11-10 08:46:17 +0100

    Display Employees component

---
+++ SandboxBlazorGit/SandboxBlazorGit/Data/EmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SandboxBlazorGit.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Mehrdad Kazemi"
+            },
+            new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Tina Desmet"
+            }
+        };
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
---
+++ SandboxBlazorGit/SandboxBlazorGit/Data/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SandboxBlazorGit.Data
+{
+    interface IEmployeeService
+    {
+        List<Employee> GetEmployees();
+    }
+}
